d47f5215cf73532aa5e556ae662a7eabdb023189
Kate <nemkovich.kate@gmail.com>
2022-08-15 23:11:31 +0300

    Introduction

---
+++ HazardParser/InternalsForExtraction.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace HazardParser;
+
+public class InternalsForExtraction
+{
+    [JsonProperty(propertyName: "datasets")]
+    public List<NameAndUnitAndGroup> Datasets { get; set; }
+}
+
+public class NameAndUnitAndGroup
+{
+    [JsonProperty(propertyName: "name")] public string Name { get; set; }
+    [JsonProperty(propertyName: "group")] public string Group { get; set; }
+    [JsonProperty(propertyName: "unit")] public string Unit { get; set; }
+    [JsonProperty(propertyName: "data")] public DataValue DValue { get; set; }
+}
+
+public class DataValue
+{
+    [JsonProperty(propertyName: "value")] public double Value { get; set; }
+}
---
+++ HazardParser/Program.cs
@@ -0,0 +1,6 @@
+﻿using HazardParser;
+using Newtonsoft.Json;
+
+var text = File.ReadAllText(@"C:\Users\Cate\Desktop\Прога\HazardParser\HazardParser\windspeed.json");
+var restoredwind = JsonConvert.DeserializeObject<InternalsForExtraction>(text);
+Console.ReadLine();
