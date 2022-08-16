771aa68391e5ced49df145196e31b0d8a6a376aa
Kate <nemkovich.kate@gmail.com>
2022-08-16 22:59:14 +0300

    LINQ

--- HazardParser/Program.cs
+++ HazardParser/Program.cs
@@ -1,6 +1,19 @@
-﻿using HazardParser;
+﻿using System.Data;
+using HazardParser;
 using Newtonsoft.Json;
+using System.Linq;
+using System.Linq.Expressions;
+
 
 var text = File.ReadAllText(@"C:\Users\Cate\Desktop\Прога\HazardParser\HazardParser\windspeed.json");
-var restoredwind = JsonConvert.DeserializeObject<InternalsForExtraction>(text);
+var restoredWind = JsonConvert.DeserializeObject<InternalsForExtraction>(text)!;
+var groupByYear=restoredWind.Datasets
+    .Where(name=> //Это была строка .Where(name=>name.Group.StartsWith("Risk Category")||name=>name.Group==("ASCE 7-05"))
+    {
+        if (name.Name.StartsWith("Risk Category")) return true;
+        if(name.Group == ("ASCE 7-05")) return true;
+        return false;
+    })
+    .GroupBy(x=>x.Group)
+    .ToList();
 Console.ReadLine();
