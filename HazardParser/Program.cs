6a87a493f37513351cacec1ff51c71364b1355c3
Kate <nemkovich.kate@gmail.com>
2022-08-18 23:19:32 +0300

    With information lists

--- HazardParser/Program.cs
+++ HazardParser/Program.cs
@@ -11,9 +11,38 @@ var groupByYear=restoredWind.Datasets
     .Where(name=> //Это была строка .Where(name=>name.Group.StartsWith("Risk Category")||name=>name.Group==("ASCE 7-05"))
     {
         if (name.Name.StartsWith("Risk Category")) return true;
-        if(name.Group == ("ASCE 7-05")) return true;
+        if(name.Group == "ASCE 7-05") return true;
         return false;
     })
     .GroupBy(x=>x.Group)
     .ToList();
-Console.ReadLine();
+Console.WriteLine("Введите год.Доступный формат ввода: 2016,16, ASCE 7-16");
+var isFound = false; // можно и  bool isFound;
+List<NameAndUnitAndGroup> information;
+while (!isFound)
+{
+   var enterInfo = Console.ReadLine();
+   foreach (var groups in groupByYear)
+   {
+       if (groups.Key == enterInfo)
+       {
+           isFound = true;
+           information=groups.ToList();
+           break;
+       }
+       
+       if (groups.Key.Substring(groups.Key.Length - 2)==enterInfo)
+       {
+           isFound = true; 
+           information=groups.ToList();
+           break;
+       }
+
+       if (groups.Key.Substring(groups.Key.Length - 2).Insert(0,"20")==enterInfo) // можно и "20"groups.Key.Substring... и $"20{groups.Key.Substring...}" ;
+       {
+           isFound = true;
+           information=groups.ToList();
+           break;
+       }
+   }
+}
