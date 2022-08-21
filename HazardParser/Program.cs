a7f69e89c5eb1d27e540ed686dedb6cd8201fd2a
Kate <nemkovich.kate@gmail.com>
2022-08-21 23:00:51 +0300

    Without table

--- HazardParser/Program.cs
+++ HazardParser/Program.cs
@@ -7,42 +7,50 @@ using System.Linq.Expressions;
 
 var text = File.ReadAllText(@"C:\Users\Cate\Desktop\Прога\HazardParser\HazardParser\windspeed.json");
 var restoredWind = JsonConvert.DeserializeObject<InternalsForExtraction>(text)!;
-var groupByYear=restoredWind.Datasets
-    .Where(name=> //Это была строка .Where(name=>name.Group.StartsWith("Risk Category")||name=>name.Group==("ASCE 7-05"))
-    {
-        if (name.Name.StartsWith("Risk Category")) return true;
-        if(name.Group == "ASCE 7-05") return true;
-        return false;
-    })
-    .GroupBy(x=>x.Group)
+var groupByYear = restoredWind.Datasets
+    .Where(
+        name => //Это была строка .Where(name=>name.Group.StartsWith("Risk Category")||name=>name.Group==("ASCE 7-05"))
+        {
+            if (name.Name.StartsWith("Risk Category")) return true;
+            if (name.Group == "ASCE 7-05") return true;
+            return false;
+        })
+    .GroupBy(x => x.Group)
     .ToList();
 Console.WriteLine("Введите год.Доступный формат ввода: 2016,16, ASCE 7-16");
-var isFound = false; // можно и  bool isFound;
-List<NameAndUnitAndGroup> information;
-while (!isFound)
+List<NameAndUnitAndGroup> result;
+while (true)
 {
-   var enterInfo = Console.ReadLine();
-   foreach (var groups in groupByYear)
-   {
-       if (groups.Key == enterInfo)
-       {
-           isFound = true;
-           information=groups.ToList();
-           break;
-       }
-       
-       if (groups.Key.Substring(groups.Key.Length - 2)==enterInfo)
-       {
-           isFound = true; 
-           information=groups.ToList();
-           break;
-       }
+    var query = Console.ReadLine();
+    if (TryFindRecords(groupByYear, query, out result)) break;
+}
 
-       if (groups.Key.Substring(groups.Key.Length - 2).Insert(0,"20")==enterInfo) // можно и "20"groups.Key.Substring... и $"20{groups.Key.Substring...}" ;
-       {
-           isFound = true;
-           information=groups.ToList();
-           break;
-       }
-   }
+Console.WriteLine(result.Count);
+
+bool TryFindRecords(List<IGrouping<string, NameAndUnitAndGroup>> records, string query,
+    out List<NameAndUnitAndGroup> result)
+{
+    foreach (var groups in records)
+    {
+        if (groups.Key == query)//Эти три условия можно записать через оператор ||
+        {
+            result = groups.ToList();
+            return true;
+        }
+
+        if (groups.Key.Substring(groups.Key.Length - 2) == query)
+        {
+            result = groups.ToList();
+            return true;
+        }
+
+        if (groups.Key.Substring(groups.Key.Length - 2).Insert(0, "20") ==
+            query) // можно и "20"groups.Key.Substring... и $"20{groups.Key.Substring...}" ;
+        {
+            result = groups.ToList();
+            return true;
+        }
+    }
+    result = null;
+    return false;
 }
---
+++ HazardParser/TableWindReport.cs
@@ -0,0 +1,19 @@
+﻿namespace HazardParser;
+
+public class TableWindReport
+{
+    public List<KeyValuePair<string, string>> _table;
+
+    public void  AddRecord()
+    {
+        
+    }
+    
+    private TableWindReport()
+    {
+        _table = new List<KeyValuePair<string, string>>();
+        _table.AddRecord();
+    }
+
+    
+}
